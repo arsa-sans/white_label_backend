4637f2e9f1494cbf9385f038445e8a10af0468d7
arsa-sans <arsaprayata72@gmail.com>
2026-09-01 15:04:22 +0700

    chore(test): refine jest testMatch pattern to src test files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   moduleNameMapper: {
     '^tslib$': '<rootDir>/node_modules/tslib/tslib.js',
